4daf8261af727bc7e8c63eaa0c333e6a55502b58
LIVIA MATIAS DE OLIVEIRA <04349051007@senacrs.edu.br>
2026-03-23 14:36:43 -0300

    subindo os arquivos para GitHub

---
+++ Variaveis/juntarTexto.js
@@ -0,0 +1,20 @@
+// =====================================
+// CONCATENAÇÃO
+// ===================================
+
+let nome= "Jurandir naoseinada dos Santos gonçalves silva kovalski nunes miller cabral terceiro machado carvalho costa niuma da costa ribeiro"
+let idade = 62
+
+console.log("Óla, meu nome é " + nome + " e tenho " + idade + " anos! Com esse nome" + nome + "maravilhoso demais, eu consigo muitas coisas")
+
+
+
+
+// ============================================================
+// TEMPLATE STRING 
+// ============================================================
+
+const nome2 = "Bartolomeu"
+const idade2 = 1
+
+console.log(`Olá, meu nome é ${nome2}) e tenho ${idade2} anos.`)
---
+++ Variaveis/variaveis01.js
@@ -0,0 +1,55 @@
+/* console.log("Hello World!!!!!!")
+console.log("Eu adoro meuzaulunos!")
+
+// =====================================
+// 1. DECLARAÇAO DE VARIAVEIS 
+// =====================================
+
+/* 
+Em JavaScript podemos declarar variaveis usando:
+let (que pode mudar)
+Constant (constante, nao pode ser reatribuida) */
+
+let nome = "Prof. Dalvana"
+let idade = 15
+const pais = "Brasil"
+
+//console.log(nome)
+//console.log(idade)
+//console.log(pais)
+// Reatribuindo valores
+ nome = "gustavo"
+ idade = 16
+ // pais = "Venezuela"
+ console.log(nome)
+ console.log(idade)
+ // console.log(pais)
+
+ //  =======================================
+ // 2. TIPO DE VARIÁVEL: NUMBER (NUMERO)
+ // ========================================
+ let numeroInteiro = 10
+ const numeroDecimal = 3.14
+
+ console.log(numeroDecimal)
+ console.log(numeroInteiro)
+
+ // ======================================
+ // 3. TIPO DE VARIÁVEL: STRING (TEXTO)
+ // ======================================
+ let nome2 = "Romário"
+ let mensagem = "Olá, eu sou um Romário aleatório"
+ 
+ console.log(nome2)
+ console.log(mensagem)
+
+ // ===================================================
+ // 4.TIPO DE VARIÁVEL: BOOLEAN (VERDADEIRO OU FALSO)
+ // ====================================================
+ let estaChovendo = true
+ let alunoReprovado = false
+
+ console.log(estaChovendo)
+ console.log(alunoReprovado)
+
+ 
